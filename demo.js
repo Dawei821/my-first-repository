d583c3bc0c7f8fbdcae6d9210ba4e79d47a6c08f
Dawei Jin <1041920143@qq.com>
2021-05-26 22:23:55 +0800

    first_update_file

---
+++ demo.js
@@ -0,0 +1,7 @@
+let box = document.getElementsByClassName('box')[0]
+window.onload = function(){
+	let box = document.getElementsByClassName('box')[0]
+	box.addEventListener('click',() =>{
+		alert('this is test code') 
+	})
+}
